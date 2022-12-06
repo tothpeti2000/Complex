482a301793b123643d154c5aa8adf9bf7379889e
tothpeti2000 <toth.peti26@gmail.com>
2022-12-06 15:52:29 +0100

    Initial commit

---
+++ src/test/kotlin/ExampleTest.kt
@@ -0,0 +1,11 @@
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleTest {
+
+    @Test
+    fun `two plus three equals five`() {
+        assertEquals(5, 2 + 3)
+    }
+
+}
